1a4fdecb006f08f546a444429b7db757907fb9a5
unknown <OBERSTEI@mu717013.rsint.net>
2014-03-07 15:38:27 +0100

    some work on serial communication

--- software/python/ayab_communication.py
+++ software/python/ayab_communication.py
@@ -1,44 +1,37 @@
 # -*- coding: utf-8 -*-
 
-import os
 import serial
 from serial.tools import list_ports
 
 
 class ayabCommunication(object):
-   def __init__(self):
-      self.__API_VERSION  = 0x02
-      self.__serInterface = 'ttyACM0'
-      pass
+   def __init__(self, pPortname):
+      self.__portname = pPortname
+
+   def __del__(self): 
+      self.closeSerial()
       
    def openSerial(self):
       try:
-        self.__ser = serial.Serial('/dev/'+self.__serInterface, 115200)
+        self.__ser = serial.Serial(self.__portname, 115200)
       except serial.SerialException:
-        print "could not open serial port"
+        print "E: could not open serial port " + self.__portname
+        return False
+      return True
+
       
    def closeSerial(self):
-      self.__ser.close
-            
-   def listSerialPorts(self):
-       """
-       Returns a generator for all available serial ports
-       """
-       if os.name == 'nt':
-           # windows
-           for i in range(256):
-               try:
-                   s = serial.Serial(i)
-                   s.close()
-                   yield 'COM' + str(i + 1)
-               except serial.SerialException:
-                   pass
-       else:
-           # unix
-           for port in list_ports.comports():
-               yield port[0]
-   def setSerialPort(pInterface):
-      self.__serInterface = pInterface
+      try:
+        self.__ser.close()
+      except:
+        pass
+
+
+   def readLine(self):
+      line = ''
+      while self.__ser.inWaiting() > 0:
+          line += self.__ser.read(1)
+      return line
 
 
    def reqStart(self):
@@ -47,12 +40,14 @@ class ayabCommunication(object):
       msg += chr(int(StopNeedle))
       msg += chr(int(StartLine))
       print "< reqStart"
-      __ser.write(msg + '\n\r')
+      self.__ser.write(msg + '\n\r')
    
+
    def reqInfo(self):
       print "< reqInfo"
-      __ser.write(chr(0x03) + '\n\r')
+      self.__ser.write(chr(0x03) + '\n\r')
       
+
    def cnfLine(self,lineNumber, lineData, flags, crc8):
       msg  = chr(0x42)                    # msg id
       msg += chr(lineNumber)              # line number
@@ -60,4 +55,4 @@ class ayabCommunication(object):
       msg += chr(flags)                   # flags
       msg += chr(crc8)                    # crc8
       print "< cnfLine"
-      __ser.write(msg + '\n\r')
+      self.__ser.write(msg + '\n\r')
