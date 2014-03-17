a870e2886fb97f8e593dc4e84161832799315cc3
chris007 <mail@chris007.de>
2014-03-17 22:40:30 +0100

    implemented internal data conversion

--- software/python/ayab_image.py
+++ software/python/ayab_image.py
@@ -3,7 +3,9 @@
 import Image
 
 class ayabImage(object):
-  def __init__(self, pFilename):
+  def __init__(self, pFilename, pNumColors):
+    self.__numColors      = pNumColors
+
     self.__imgPosition    = 'center'
     self.__imgStartNeedle = '0'
     self.__imgStopNeedle  = '0'
@@ -14,19 +16,17 @@ class ayabImage(object):
     self.__startLine  = 0
     self.__startBlock = 0
 
-    self.__origImage = Image.open(pFilename)
+    self.__image = Image.open(pFilename)
     self.__filename  = pFilename
 
-    # TODO knitImage: convert to internal Dataformat
-    self.__knitImage = self.__origImage
-    self.__knitImage = self.__knitImage.convert('1') # convert to 1 bit depth
-    self.__calcImgStartStopNeedles()
+    self.__image = self.__image.convert('L') # convert to 1 byte depth
+    self.__updateImageData()
 
   def filename(self):
     return self.__filename
 
-  def knitImage(self):
-    return self.__knitImage
+  def image(self):
+    return self.__image
 
   def knitStartNeedle(self):
     return self.__knitStartNeedle
@@ -49,71 +49,71 @@ class ayabImage(object):
   def startBlock(self):
     return self.__startBlock
 
-  def imageToIntern(self):    
-    self.__knitData4Color = \
-      [[0 for i in range(self.__knitImage.size[0])] \
-      for j in range(self.__knitImage.size[1])]
+  def __updateImageData(self):
+    self.__imgWidth   = self.__image.size[0]
+    self.__imgHeight  = self.__image.size[1]      
 
-    # Distill image to 4 colors
-    for y in range(0, self.__knitImage.size[1]):
-      msg = ''
-      for x in range(0, self.__knitImage.size[0]):
-        pxl = self.__knitImage.getpixel((x, y))
-        if pxl >= 0 and pxl < 64: # color A
-          msg += '-'
-          self.__knitData4Color[y][x] = 0
-        elif pxl >= 64 and pxl < 128:
-          msg += ':'
-          self.__knitData4Color[y][x] = 1
-        elif pxl >= 128 and pxl < 192:
-          msg += '#'
-          self.__knitData4Color[y][x] = 2
-        elif pxl >= 192 and pxl < 256:
-          msg += '+'
-          self.__knitData4Color[y][x] = 3
-      print msg
+    self.__convertImgToIntern()
+    self.__calcImgStartStopNeedles()
 
-    print(self.__knitData4Color)
+  def __convertImgToIntern(self): 
+    num_colors = self.__numColors
+    clr_range  = float(256)/num_colors
 
-    print len(self.__knitData4Color) #row
-    print len(self.__knitData4Color[0]) #col
-    self.__knitDataExpanded = \
-      [[0 for i in range(len(self.__knitData4Color[0]))] \
-      for j in range(4*len(self.__knitData4Color))]
+    imgWidth   = self.__imgWidth
+    imgHeight  = self.__imgHeight
+    
+    self.__imageIntern = \
+      [[0 for i in range(imgWidth)] \
+      for j in range(imgHeight)]
+    self.__imageColors = \
+      [[0 for i in range(num_colors)] \
+      for j in range(imgHeight)]
+    self.__imageExpanded = \
+      [[0 for i in range(imgWidth)] \
+      for j in range(4*imgHeight)]
 
-    print len(self.__knitDataExpanded) #row
-    print len(self.__knitDataExpanded[0]) #col
-      # Expand knitData to a line per color
-    for y in range(len(self.__knitData4Color)):
-      msg = ''
-      # Extract colors from each line
-      # TODO implement
-      for x in range(len(self.__knitData4Color[0])):
-        # Set pixels in current color-line
-        # TODO implement
-        msg += str(self.__knitDataExpanded[y][x])
-      print msg
+    # Distill image to x colors
+    for row in range(0, imgHeight):
+      for col in range(0, imgWidth):
+        pxl = self.__image.getpixel((col, row))
+
+        for color in range(0, num_colors):
+          lowerBound = int(color*clr_range)
+          upperBound = int((color+1)*clr_range) 
+          if pxl>=lowerBound and pxl<upperBound:
+            # color map
+            self.__imageIntern[row][col]    = color
+            # amount of bits per color per line
+            self.__imageColors[row][color]  += 1
+            # colors separated per line
+            self.__imageExpanded[(4*row)+color][col] = 1
+    
+    # print(self.__imageIntern)
+    # print(self.__imageColors)
+    # print(self.__imageExpanded)
+
 
   def __calcImgStartStopNeedles(self):
     if self.__imgPosition == 'center':
         needleWidth = (self.__knitStopNeedle - self.__knitStartNeedle) +1
-        self.__imgStartNeedle = (self.__knitStartNeedle + needleWidth/2) - self.__knitImage.size[0]/2
-        self.__imgStopNeedle  = self.__imgStartNeedle + self.__knitImage.size[0] -1      
+        self.__imgStartNeedle = (self.__knitStartNeedle + needleWidth/2) - self.__image.size[0]/2
+        self.__imgStopNeedle  = self.__imgStartNeedle + self.__image.size[0] -1      
 
     elif self.__imgPosition == 'left':
         self.__imgStartNeedle = self.__knitStartNeedle
-        self.__imgStopNeedle  = self.__imgStartNeedle + self.__knitImage.size[0]
+        self.__imgStopNeedle  = self.__imgStartNeedle + self.__image.size[0]
 
     elif self.__imgPosition == 'right':
-        self.__imgStopNeedle = self.__knitStopNeedle
-        self.__imgStartNeedle = self.__imgStopNeedle - self.__knitImage.size[0]
+        self.__imgStopNeedle  = self.__knitStopNeedle
+        self.__imgStartNeedle = self.__imgStopNeedle - self.__image.size[0]
 
     elif int(self.__imgPosition) > 0 and int(self.__imgPosition) < 200:
         self.__imgStartNeedle = int(self.__imgPosition)
-        self.__imgStopNeedle  = self.__imgStartNeedle + self.__knitImage.size[0]
+        self.__imgStopNeedle  = self.__imgStartNeedle + self.__image.size[0]
 
     else:
-        print "unknown alignment"
+        # print "unknown alignment"
         return False
     return True
 
@@ -122,13 +122,14 @@ class ayabImage(object):
       """
       invert the pixels of the image
       """
-      for y in range(0, self.__knitImage.size[1]):
-        for x in range(0, self.__knitImage.size[0]):
-          pxl = self.__knitImage.getpixel((x, y))
+      for y in range(0, self.__image.size[1]):
+        for x in range(0, self.__image.size[0]):
+          pxl = self.__image.getpixel((x, y))
           if pxl == 255:
-            self.__knitImage.putpixel((x,y),0)
+            self.__image.putpixel((x,y),0)
           else:
-            self.__knitImage.putpixel((x,y),255)
+            self.__image.putpixel((x,y),255)
+      self.__updateImageData()
       
 
   def rotateImage(self):
@@ -136,20 +137,20 @@ class ayabImage(object):
       rotate the image 90 degrees clockwise
       """
       print "rotating image 90 degrees..."
-      self.__knitImage = self.__knitImage.rotate(-90)
+      self.__image = self.__image.rotate(-90)
 
-      self.__calcImgStartStopNeedles()
+      self.__updateImageData()
 
 
   def resizeImage(self, pNewWidth):
       """
       resize the image to a given width, keeping the aspect ratio
       """
-      wpercent = (pNewWidth/float(self.__knitImage.size[0]))
-      hsize = int((float(self.__knitImage.size[1])*float(wpercent)))
-      self.__knitImage = self.__knitImage.resize((pNewWidth,hsize), Image.ANTIALIAS)
+      wpercent = (pNewWidth/float(self.__image.size[0]))
+      hsize = int((float(self.__image.size[1])*float(wpercent)))
+      self.__image = self.__image.resize((pNewWidth,hsize), Image.ANTIALIAS)
 
-      self.__calcImgStartStopNeedles()
+      self.__updateImageData()
 
 
   def setKnitNeedles(self, pKnitStart, pKnitStop):
@@ -173,7 +174,7 @@ class ayabImage(object):
       set the line where to start knitting
       """
       #Check if StartLine is in valid range (picture height)
-      if pStartLine >= self.__knitImage.size[1]:
+      if pStartLine >= self.__image.size[1]:
           return
 
       self.__startLine = pStartLine    
