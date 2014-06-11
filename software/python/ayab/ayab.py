f7dc16270825cbe5a77069d116175815e6ef3fc8
Sebastian Oliva <code@sebastianoliva.com>
2014-06-11 14:49:21 -0600

    added launcher

--- software/python/ayab/ayab.py
+++ software/python/ayab/ayab.py
@@ -17,26 +17,17 @@
 #    Copyright 2014 Sebastian Oliva, Christian Obersteiner, Andreas Müller
 #    https://bitbucket.org/chris007de/ayab-apparat/
 
-"""Provides an Interface for users to operate AYAB using a GUI.
-
-This module handles serial communication, currently works in a synchronous way.
-AyabCommunication uses an internal PySerial.Serial object to connect to the device.
-The initializer can also be overriden with a dummy serial object.
-"""
+"""Provides an Interface for users to operate AYAB using a GUI."""
 
 import sys
 from PyQt4 import QtGui
 from ayab_gui import Ui_Form
 
 
-class MainWindow(QtGui.QMainWindow):
-    def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
-
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
