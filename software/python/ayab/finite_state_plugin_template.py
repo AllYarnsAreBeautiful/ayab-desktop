058d45d8dd3b49d4b5c2f85461d31b25f03f0ddd
Sebastian Oliva <code@sebastianoliva.com>
2014-06-13 01:31:28 -0600

    added template for finite state machine plugins.

---
+++ software/python/ayab/finite_state_plugin_template.py
@@ -0,0 +1,41 @@
+from fysom import Fysom
+
+
+def onconfigure(e):
+    '''Sets configuration for knitting session.'''
+    assert e.options
+    pass
+
+
+def onknit(e):
+    '''Handles transition to knitting stage. Sets up callbacks for control.'''
+    pass
+
+
+def onknitting(e):
+    """Handles the automated knitting process."""
+    ## For AYAB, add here the knittting loop currently inside ayab_control.knitImage
+    pass
+
+
+def onfinish(e):
+    #TODO: do cleanup.
+    pass
+
+fsm = Fysom(
+    {'initial': 'activated',
+     'events': [
+         {'name': 'configure', 'src': 'activated', 'dst': 'configured'},
+         {'name': 'knit', 'src': 'configured', 'dst': 'knitting'},
+         {'name': 'finish', 'src': 'knitting', 'dst': 'finished'},
+     ],
+     'callbacks': {
+         'onknit': onknit,
+         'onknitting': onknitting,
+         'onconfigure': onconfigure,
+         'onfinish': onfinish
+     }})
+
+fsm.configure(image=None, options={})
+fsm.knit()
+fsm.finish()
