7fbb63f9c09badaced5ebbaf743225cb587caede
Sebastian Oliva <code@sebastianoliva.com>
2014-06-17 16:01:22 -0600

    basic plugin infrastructure

---
+++ software/python/ayab/plugins/ayab_plugin/ayab_control.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#This file is part of AYAB.
+#
+#    AYAB is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    AYAB is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with AYAB.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Copyright 2013 Christian Obersteiner, Andreas Müller
+#    https://bitbucket.org/chris007de/ayab-apparat/
+
+from ayab_communication import AyabCommunication
+import time
+from plugins.knitting_plugin import KnittingPlugin
+
+
+class AyabPluginControl(KnittingPlugin):
+  def __init__(self):
+    super(AyabPluginControl, self).__init__()
+    #KnittingPlugin.__init__(self)
