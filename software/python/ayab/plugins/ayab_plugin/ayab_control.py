92644db71dc653aefaf52eaa8ed8b945f78c5622
Sebastian Oliva <code@sebastianoliva.com>
2014-06-17 21:02:42 -0600

    Testing control with delayed synchronous onknit method

--- software/python/ayab/plugins/ayab_plugin/ayab_control.py
+++ software/python/ayab/plugins/ayab_plugin/ayab_control.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#This file is part of AYAB.
+# This file is part of AYAB.
 #
 #    AYAB is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with AYAB.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    Copyright 2013 Christian Obersteiner, Andreas Müller
+#    Copyright 2013, 2014 Sebastian Oliva, Christian Obersteiner, Andreas Müller
 #    https://bitbucket.org/chris007de/ayab-apparat/
 
 from ayab_communication import AyabCommunication
@@ -23,6 +23,19 @@ from plugins.knitting_plugin import KnittingPlugin
 
 
 class AyabPluginControl(KnittingPlugin):
+
+  def onknit(self, e):
+    try:
+        for i in range(e.many):
+          print i
+          time.sleep(1)
+        return True
+    except:
+        return False
+
   def __init__(self):
-    super(AyabPluginControl, self).__init__()
-    #KnittingPlugin.__init__(self)
+    callbacks_dict = {
+        'onknit': self.onknit,
+    }
+    super(AyabPluginControl, self).__init__(callbacks_dict)
+    # KnittingPlugin.__init__(self)
