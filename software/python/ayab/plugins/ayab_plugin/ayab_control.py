6873f01db89f3d39b4a852539c66a6540d7d9bb3
Sebastian Oliva <code@sebastianoliva.com>
2014-06-22 22:19:45 -0600

    filling missing methods on ayab_control

--- software/python/ayab/plugins/ayab_plugin/ayab_control.py
+++ software/python/ayab/plugins/ayab_plugin/ayab_control.py
@@ -20,18 +20,57 @@
 from ayab_communication import AyabCommunication
 import time
 from plugins.knitting_plugin import KnittingPlugin
+from PyQt4 import QtGui
 
 
 class AyabPluginControl(KnittingPlugin):
 
   def onknit(self, e):
-    try:
-        for i in range(e.many):
+    #try:
+        for i in range(self._cycle_ammount):
           print i
+          self.__wait_for_user_action()
           time.sleep(1)
         return True
-    except:
+    #except:
         return False
+
+  def onconfigure(self, e):
+    assert e.parent_ui
+    self.__parent_ui = e.parent_ui
+    self._cycle_ammount = 3
+    return
+
+  def onfinish(self, e):
+    pass
+
+  def __wait_for_user_action(self):
+    ret = QtGui.QMessageBox.warning(
+        self.__parent_ui,
+        "AYAB",
+        "INIT MACHINE.\n",
+        QtGui.QMessageBox.AcceptRole,
+        QtGui.QMessageBox.AcceptRole)
+    if ret == QtGui.QMessageBox.AcceptRole:
+      return True
+
+  def setup_ui(self, ui):
+    pass
+
+  def get_configuration_from_ui(self, ui):
+    self.conf = {}
+    start_line_text = ui.findChild(QtGui.QLineEdit, "start_line_edit").text()
+    self.conf["start_line"] = int(start_line_text)
+    start_needle_text = ui.findChild(QtGui.QLineEdit, "start_needle_edit").text()
+    self.conf["start_needle"] = int(start_needle_text)
+    stop_needle_text = ui.findChild(QtGui.QLineEdit, "stop_needle_edit").text()
+    self.conf["stop_needle"] = int(stop_needle_text)
+    alignment_text = ui.findChild(QtGui.QComboBox, "alignment_combo_box").currentText()
+    self.conf["alignment"] = alignment_text
+    machine_type_text = ui.findChild(QtGui.QComboBox, "machine_type_box").currentText()
+    self.conf["machine_type"] = machine_type_text
+    #TODO: add more config options
+    return self.conf
 
   def __init__(self):
     callbacks_dict = {
