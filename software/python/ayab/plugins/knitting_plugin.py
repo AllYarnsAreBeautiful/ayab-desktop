ec790e5b0cb500ab5b39eb13dc7260426d1c14b9
Sebastian Oliva <code@sebastianoliva.com>
2014-06-23 00:30:49 -0600

    more methods required to be implemented on KnittingPlugin

--- software/python/ayab/plugins/knitting_plugin.py
+++ software/python/ayab/plugins/knitting_plugin.py
@@ -9,14 +9,24 @@ class KnittingPlugin(IPlugin, Fysom):
   '''A generic plugin implementing a state machine for knitting.'''
 
   def onknit(self, e):
-    """Called when state machine executes knit()"""
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onknit is used for the main 'knitting loop'"))
+    """Callback when state machine executes knit()"""
+    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onknit is used for the main 'knitting loop'."))
 
   def onfinish(self, e):
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onfinish is a callback that is called when knitting is over"))
+    """Callback when state machine executes finish()"""
+    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onfinish is a callback that is called when knitting is over."))
 
   def onconfigure(self, e):
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onconfigure is used to configure the knitting plugin before starting"))
+    """Callback when state machine executes configure(parent_ui = parent, options={})"""
+    assert e.parent_ui
+    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onconfigure is used to configure the knitting plugin before starting."))
+
+  def setup_ui(self, ui):
+    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("setup_ui is used to load the knitting_options_dock panel ui for the plugin."))
+
+  def get_configuration_from_ui(self, ui):
+    """Loads options dict with a given parent QtGui object."""
+    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("get_configuration_from_ui loads options with a given parent ui object."))
 
   def __init__(self, callbacks_dict):
     """
