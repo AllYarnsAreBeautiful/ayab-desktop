af0f5d9aa139f4037e8ef2947335954f36e3a62f
Sebastian Oliva <code@sebastianoliva.com>
2014-06-30 01:55:45 -0600

    fixin non accessible __NOT_IMPLEMENTED_ERROR

--- software/python/ayab/plugins/knitting_plugin.py
+++ software/python/ayab/plugins/knitting_plugin.py
@@ -2,43 +2,41 @@ from yapsy.IPlugin import IPlugin
 from fysom import Fysom
 
 
-__NOT_IMPLEMENTED_ERROR = "Classes that inherit from KnittingPlugin should implment {0}"
-
-
 class KnittingPlugin(IPlugin, Fysom):
   '''A generic plugin implementing a state machine for knitting.'''
 
   def onknit(self, e):
     """Callback when state machine executes knit()"""
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onknit. It is used for the main 'knitting loop'."))
+    raise NotImplementedError(self.__NOT_IMPLEMENTED_ERROR.format("onknit. It is used for the main 'knitting loop'."))
 
   def onfinish(self, e):
     """Callback when state machine executes finish()"""
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onfinish. It is a callback that is called when knitting is over."))
+    raise NotImplementedError(self.__NOT_IMPLEMENTED_ERROR.format("onfinish. It is a callback that is called when knitting is over."))
 
   def onconfigure(self, e):
     """Callback when state machine executes configure(parent_ui = parent, options={})"""
-    assert e.parent_ui
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("onconfigure. It is used to configure the knitting plugin before starting."))
+    raise NotImplementedError(self.__NOT_IMPLEMENTED_ERROR.format("onconfigure. It is used to configure the knitting plugin before starting."))
 
   def setup_ui(self, ui):
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("setup_ui. It loads the knitting_options_dock panel ui for the plugin."))
+    raise NotImplementedError(self.__NOT_IMPLEMENTED_ERROR.format("setup_ui. It loads the knitting_options_dock panel ui for the plugin."))
 
   def cleanup_ui(self, ui):
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("cleanup_ui. It cleans up the knitting_options_dock panel ui for the plugin."))
+    raise NotImplementedError(self.__NOT_IMPLEMENTED_ERROR.format("cleanup_ui. It cleans up the knitting_options_dock panel ui for the plugin."))
 
   def get_configuration_from_ui(self, ui):
     """Loads options dict with a given parent QtGui object."""
-    raise NotImplementedError(__NOT_IMPLEMENTED_ERROR.format("get_configuration_from_ui. It loads options with a given parent ui object."))
+    raise NotImplementedError(self.__NOT_IMPLEMENTED_ERROR.format("get_configuration_from_ui. It loads options with a given parent ui object."))
 
   def __init__(self, callbacks_dict):
     """
     Args:
         _fsm: The internal finite state machine.
     """
+    self.__NOT_IMPLEMENTED_ERROR = "Classes that inherit from KnittingPlugin should implment {0}"
+
     callbacks_dict = {
         'onknit': self.onknit,
-       # 'onknitting': self.onknitting,
+        #'onknitting': self.onknitting,
         'onconfigure': self.onconfigure,
         'onfinish': self.onfinish,
         }
