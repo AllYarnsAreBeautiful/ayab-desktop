65d945d56c4dff60a28b34c46181a200d0a8f970
Sebastian Oliva <code@sebastianoliva.com>
2014-07-11 00:00:34 -0600

    Improvements to firmware flash Ui

---
+++ software/python/ayab/firmware_flash_ui.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'firmware_flash.ui'
+#
+# Created by: PyQt4 UI code generator 4.10.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Frame(object):
+    def setupUi(self, Frame):
+        Frame.setObjectName(_fromUtf8("Frame"))
+        Frame.resize(672, 386)
+        Frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        Frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.horizontalLayout = QtGui.QHBoxLayout(Frame)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.widget = QtGui.QWidget(Frame)
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout_2.setMargin(0)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.textBrowser = QtGui.QTextBrowser(self.widget)
+        self.textBrowser.setHtml(_fromUtf8("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:18px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:xx-large; font-weight:600;\">Firmware Install Guide</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas, lacus sed tincidunt tristique, nisl lacus suscipit felis, sed mattis risus sem quis sem. Fusce lorem purus, pharetra sed dapibus non, placerat ac sem. In eget varius massa. Cras vel blandit erat, at fermentum quam. Vivamus ipsum sem, elementum consequat lorem semper, posuere varius tortor. Sed vel ornare nulla. Ut a metus ut lacus elementum feugiat. Pellentesque sed vulputate dolor. Aliquam sem risus, dictum non commodo id, ullamcorper suscipit nisi. Aliquam quis fermentum sem. Phasellus felis velit, consequat sed convallis a, tempor quis ante. Ut placerat eleifend eleifend. Sed leo mi, gravida in vulputate vitae, luctus at neque. Vivamus in hendrerit ligula. Pellentesque non cursus tellus. Suspendisse rhoncus felis sit amet purus scelerisque, eu scelerisque ligula suscipit. Aenean accumsan ligula ante. Donec interdum magna vel enim adipiscing, sed dictum lacus accumsan. Praesent laoreet, libero quis pretium tincidunt, augue odio fermentum justo, in iaculis ligula urna ac nulla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu lectus a elit fermentum condimentum. Nullam tincidunt consequat dui facilisis tincidunt. Quisque et nisl facilisis, fermentum ante sit amet, facilisis velit. Nunc sed ligula et mi ultricies blandit nec in risus. Nulla at tortor urna. Proin iaculis fringilla est sit amet semper. Nam luctus dui faucibus blandit tempus. Quisque at tempus libero. Donec bibendum eleifend dui, id rutrum libero venenatis ut. Pellentesque id odio est. Vivamus rutrum scelerisque adipiscing. Nunc et sapien ut urna eleifend mattis vitae vitae sapien. Nulla iaculis quis metus et tincidunt. In venenatis elit a turpis mollis, ut ornare est consectetur. Maecenas tellus ante, tincidunt vitae ligula a, euismod aliquet nibh. Fusce tempus accumsan ante, eu mattis odio lobortis vel. Ut non aliquam tellus, sed faucibus justo. Cras sodales felis sem, ut semper metus tempus in. Etiam pulvinar dapibus ullamcorper. Morbi ut facilisis purus. Pellentesque tempor sed turpis id tempor. Etiam dictum lectus a velit tempus, vitae convallis purus interdum. Maecenas consectetur condimentum risus, quis tempus velit consectetur id. Aliquam ut nulla enim. Vestibulum eget lacinia magna. Fusce convallis, elit ac tincidunt gravida, tellus purus posuere diam, iaculis pharetra urna erat sit amet purus. Nullam metus erat, auctor vitae massa nec, pulvinar gravida est. Donec ullamcorper, nibh ac feugiat volutpat, lectus turpis eleifend neque, at consequat est eros a lorem. Pellentesque rutrum turpis nec eros fringilla, nec auctor leo faucibus. Proin magna ante, dignissim id massa ac, blandit eleifend tellus. Maecenas a dolor enim. Integer quam eros, dapibus in pellentesque id, lobortis vitae diam. Sed sed lorem blandit, suscipit magna a, ullamcorper metus. Nulla rhoncus quam sit amet leo blandit, in porta augue hendrerit. Phasellus adipiscing quis tortor vitae porta. Nam in venenatis sem, sit amet fringilla dolor. Suspendisse eu iaculis justo. Integer faucibus risus nec sapien venenatis, eget ultrices nulla pellentesque. Duis sagittis turpis eget pellentesque fermentum. Aliquam ac ante at odio posuere pharetra ut sed neque. Nullam blandit libero et nibh luctus, ac congue dui consectetur. Etiam erat erat, gravida in dui vitae, vestibulum iaculis libero. </p></body></html>"))
+        self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
+        self.verticalLayout_2.addWidget(self.textBrowser)
+        self.horizontalLayout.addWidget(self.widget)
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label_3 = QtGui.QLabel(Frame)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.verticalLayout.addWidget(self.label_3)
+        self.comboBox = QtGui.QComboBox(Frame)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.verticalLayout.addWidget(self.comboBox)
+        self.label = QtGui.QLabel(Frame)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        self.device_list = QtGui.QListWidget(Frame)
+        self.device_list.setObjectName(_fromUtf8("device_list"))
+        item = QtGui.QListWidgetItem()
+        self.device_list.addItem(item)
+        item = QtGui.QListWidgetItem()
+        self.device_list.addItem(item)
+        self.verticalLayout.addWidget(self.device_list)
+        self.label_2 = QtGui.QLabel(Frame)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.verticalLayout.addWidget(self.label_2)
+        self.firmware_list = QtGui.QListWidget(Frame)
+        self.firmware_list.setObjectName(_fromUtf8("firmware_list"))
+        item = QtGui.QListWidgetItem()
+        self.firmware_list.addItem(item)
+        item = QtGui.QListWidgetItem()
+        self.firmware_list.addItem(item)
+        self.verticalLayout.addWidget(self.firmware_list)
+        self.flash_firmware = QtGui.QPushButton(Frame)
+        self.flash_firmware.setObjectName(_fromUtf8("flash_firmware"))
+        self.verticalLayout.addWidget(self.flash_firmware)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Frame)
+        QtCore.QMetaObject.connectSlotsByName(Frame)
+
+    def retranslateUi(self, Frame):
+        Frame.setWindowTitle(_translate("Frame", "Firmware Flashing Utility", None))
+        self.label_3.setText(_translate("Frame", "Port", None))
+        self.label.setText(_translate("Frame", "Device", None))
+        __sortingEnabled = self.device_list.isSortingEnabled()
+        self.device_list.setSortingEnabled(False)
+        item = self.device_list.item(0)
+        item.setText(_translate("Frame", "Arduino Uno", None))
+        item = self.device_list.item(1)
+        item.setText(_translate("Frame", "Arduino Mega 2560", None))
+        self.device_list.setSortingEnabled(__sortingEnabled)
+        self.label_2.setText(_translate("Frame", "Firmware Version", None))
+        __sortingEnabled = self.firmware_list.isSortingEnabled()
+        self.firmware_list.setSortingEnabled(False)
+        item = self.firmware_list.item(0)
+        item.setText(_translate("Frame", "v3", None))
+        item = self.firmware_list.item(1)
+        item.setText(_translate("Frame", "v3-dummy", None))
+        self.firmware_list.setSortingEnabled(__sortingEnabled)
+        self.flash_firmware.setText(_translate("Frame", "Flash", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Frame = QtGui.QFrame()
+    ui = Ui_Frame()
+    ui.setupUi(Frame)
+    Frame.show()
+    sys.exit(app.exec_())
+
