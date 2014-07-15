a977b2b03f6193d754c105503eeaa53e522ea646
Sebastian Oliva <code@sebastianoliva.com>
2014-07-14 23:15:38 -0600

    Setting up Translation infrastructure for ayab base and ayab plugin.

---
+++ software/python/ayab/ayab_gui.BASE.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ayab_gui.ui" line="14"/>
+        <source>All Yarns Are Beautiful</source>
+        <translation type="unfinished">EN_TEST</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="21"/>
+        <source>Knitting Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="51"/>
+        <source>Asistant</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="61"/>
+        <source>Select Module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="97"/>
+        <source>Load File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="130"/>
+        <source>Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="137"/>
+        <source>Knit!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="181"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="226"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="195"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="206"/>
+        <source>Load AYAB Firmware</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="211"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="216"/>
+        <source>Open Knitting Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="221"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
