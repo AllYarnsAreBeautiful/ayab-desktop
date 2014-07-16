3657abfb715c32872ea86212c6d1010f4face371
Sebastian Oliva <code@sebastianoliva.com>
2014-07-15 22:48:19 -0600

    Added German translation for ayab_gui

---
+++ software/python/ayab/translations/ayab_gui.de_DE.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ayab_gui.ui" line="14"/>
+        <source>All Yarns Are Beautiful</source>
+        <translation>All Yarns Are Beautiful</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="21"/>
+        <source>Knitting Options</source>
+        <translation>Strickeinstellungen</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="51"/>
+        <source>Asistant</source>
+        <translation>Assistent</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="61"/>
+        <source>Select Module</source>
+        <translation>Modul wählen</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="97"/>
+        <source>Load File</source>
+        <translation>Datei öffnen</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="130"/>
+        <source>Control</source>
+        <translation>Bedienung</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="137"/>
+        <source>Knit!</source>
+        <translation>Stricken!</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="181"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="226"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="195"/>
+        <source>Tools</source>
+        <translation>Werkzeuge</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="206"/>
+        <source>Load AYAB Firmware</source>
+        <translation>AYAB Firmware aufspielen</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="211"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="216"/>
+        <source>Open Knitting Project</source>
+        <translation>Projekt öffnen</translation>
+    </message>
+    <message>
+        <location filename="ayab_gui.ui" line="221"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+</context>
+</TS>
