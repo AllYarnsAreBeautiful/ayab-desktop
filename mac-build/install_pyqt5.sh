31a53d55060ba82784cbc6f1261f84fde036d5fd
chris <mail@chris007.de>
2017-04-25 19:17:32 +0200

    adding sudo to make install call

--- mac-build/install_pyqt5.sh
+++ mac-build/install_pyqt5.sh
@@ -9,7 +9,7 @@ tar zxf sip-4.19.2.tar.gz
 cd sip-4.19.2
 python configure.py
 make
-make install
+sudo make install
 
 ## Install Qt5
 brew install qt5
@@ -22,10 +22,7 @@ export PATH="/Users/fu/Qt/5.4/clang_64/bin:$PATH"
 python -c "import site; print site.getsitepackages()"
 
 ## compile pyQT ...
-python configure.py \
-        #-q /Users/fu/Qt/5.4/clang_64/bin/qmake \
-        -d /Library/Python/2.7/site-packages/ \
-        --sip /System/Library/Frameworks/Python.framework/Versions/2.7/bin/sip
+python configure.py -d /Library/Python/2.7/site-packages/ --sip /System/Library/Frameworks/Python.framework/Versions/2.7/bin/sip
 
 make
 sudo make install
