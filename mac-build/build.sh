569bdf645e552435b0b6557a8363c7bd5b823dda
derchris <derchris@me.com>
2017-04-25 21:59:33 +0200

    mac: using pyenv to install python 3.5

--- mac-build/build.sh
+++ mac-build/build.sh
@@ -12,7 +12,7 @@ cd "$HERE"
   cd ..
 
   echo "# build the distribution"
-  python3 setup.py sdist
+  ~/.pyenv/shims/python3 setup.py sdist
 )
 
 pwd
@@ -22,7 +22,7 @@ echo "# build the app"
 cp ayab.spec ../
 cd ..
 # see https://pythonhosted.org/PyInstaller/usage.html
-python3 -m PyInstaller -d -y ayab.spec
+~/.pyenv/shims/python3 -m PyInstaller -d -y ayab.spec
 
 echo "# create the .dmg file"
 # see http://stackoverflow.com/a/367826/1320237
--- mac-build/install.sh
+++ mac-build/install.sh
@@ -22,12 +22,12 @@ echo "# install PyQt5"
 brew install PyQt5
 
 echo -n "Python version: "
-python3 --version
-python3 -m pip install --upgrade pip
+~/.pyenv/shims/python3 --version
+~/.pyenv/shims/python3 -m pip install --upgrade pip
 
 echo "# install requirements"
-python3 -m pip install $USER -r ../requirements.txt
-python3 -m pip install $USER PyInstaller
+~/.pyenv/shims/python3 -m pip install $USER -r ../requirements.txt
+~/.pyenv/shims/python3 -m pip install $USER PyInstaller
 
 ./build.sh $USER
 
