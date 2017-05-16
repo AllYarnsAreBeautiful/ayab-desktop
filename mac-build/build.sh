9630a436f99fb708751a593d0934dee0fd1ce931
derchris <derchris@me.com>
2017-05-17 01:18:23 +0200

    Fixed path for DMG creation

--- mac-build/build.sh
+++ mac-build/build.sh
@@ -10,16 +10,14 @@ cd "$HERE"
 
 mkdir -p ../dist/release
 
-pwd
-ls
-
 echo "# build the app"
 /usr/local/bin/platypus -P AYAB_bash.platypus -y ../dist/AYAB
 
 echo "# create the .dmg file"
 # see http://stackoverflow.com/a/367826/1320237
-AYAB_DMG="`pwd`/dist/release/AYAB.dmg"
+AYAB_DMG="`pwd`/../dist/release/AYAB.dmg"
 rm -f "$AYAB_DMG"
+ls -l ../dist/
 hdiutil create -srcfolder ../dist/AYAB.app "$AYAB_DMG"
 
 echo "The installer can be found in \"$AYAB_DMG\"."
