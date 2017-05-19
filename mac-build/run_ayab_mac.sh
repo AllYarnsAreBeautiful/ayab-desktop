7e3b165a1becab7562b4cb5f39f37665ff72ed31
Christian Gerbrandt <derchris@me.com>
2017-05-19 16:31:14 +0200

    mac start script without homebrew

--- mac-build/run_ayab_mac.sh
+++ mac-build/run_ayab_mac.sh
@@ -2,26 +2,26 @@
 export LC_CTYPE=en_US.UTF-8
 export PYTHONIOENCODING=utf-8
 
-if [ ! -f "/usr/local/bin/brew" ]
+USER=`whoami`
+
+if [ ! -f "/Users/$USER/.pyenv/bin/pyenv" ]
 then
-  echo "installing brew"
-  mkdir /tmp/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /tmp/homebrew
-  mkdir /usr/local/bin
-  cp /tmp/homebrew/bin/brew /usr/local/bin/
+  echo "install pyenv"
+  curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer -o /tmp/pyenv-installer
+  bash /tmp/pyenv-installer
+  rm -f /tmp/pyenv-installer
 fi
 
-/usr/local/bin/brew update
-/usr/local/bin/brew install pyenv
-if [ -f "~/.pyenv/shims/python3.5" ]
+if [ ! -f "/Users/$USER/.pyenv/shims/python3.5" ]
 then
-  echo "install python" 
-  env PYTHON_CONFIGURE_OPTS="--enable-framework" /usr/local/bin/pyenv install 3.5.0
+  echo "install python"
+  env PYTHON_CONFIGURE_OPTS="--enable-framework" /Users/$USER/.pyenv/bin/pyenv install 3.5.0
 fi
 
-/usr/local/bin/pyenv local 3.5.0
-eval "$(/usr/local/bin/pyenv init -)"
-~/.pyenv/shims/pip3 install --upgrade pip
-~/.pyenv/shims/pip3 install PyQt5
-~/.pyenv/shims/pip3 install -r requirements.txt
+/Users/$USER/.pyenv/bin/pyenv local 3.5.0
+eval "$(/Users/$USER/.pyenv/bin/pyenv init -)"
+/Users/$USER/.pyenv/shims/pip3 install --upgrade pip
+/Users/$USER/.pyenv/shims/pip3 install PyQt5
+/Users/$USER/.pyenv/shims/pip3 install -r requirements.txt
 
-~/.pyenv/shims/python3 ayab_devel_launch.py
+/Users/$USER/.pyenv/shims/python3 ayab_devel_launch.py
