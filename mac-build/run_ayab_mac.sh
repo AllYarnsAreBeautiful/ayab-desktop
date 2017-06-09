5fb886aab6c8c6b65c71e925d64bbe6ee98d44f6
derchris <derchris@me.com>
2017-06-09 22:00:22 +0200

    verbose option no longer available on sierra

--- mac-build/run_ayab_mac.sh
+++ mac-build/run_ayab_mac.sh
@@ -11,7 +11,7 @@ else
      echo "install xcode command line tools"
      touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
      PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-     softwareupdate -i "$PROD" -v;
+     softwareupdate -i "$PROD";
 fi
 
 if [ ! -f "/Users/$USER/.pyenv/bin/pyenv" ]
