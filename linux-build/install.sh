aa589be3abee0ed9d0d61480b33c4d6d4fe1a666
derchris <derchris@me.com>
2017-07-06 16:04:51 +0200

    modify pypi readme

--- linux-build/install.sh
+++ linux-build/install.sh
@@ -12,23 +12,17 @@ TAG_NAME="`./tag_name`"
 
 cd ..
 
-echo "# install pandoc"
-apt-get update
-apt-get -y install pandoc
-
 echo "# build the distribution"
-pandoc --from=markdown --to=rst --output=README.rst README.md
 cp package_version LICENSE.txt ayab/
-python setup.py sdist bdist_wheel
+python setup.py sdist bdist bdist_wheel
 mkdir -p dist/release
 mv dist/ayab*.tar.gz dist/release/
 mv dist/ayab*.whl dist/release/
+mv dist/ayab*.egg dist/release/
 ls dist/release/
 
 echo "# show the versions"
 echo -n "setup.py --version: "
 python setup.py --version
-echo -n "requirements: "
-python setup.py --requires
 echo "Package version $PACKAGE_VERSION with possible tag name $TAG_NAME"
 
