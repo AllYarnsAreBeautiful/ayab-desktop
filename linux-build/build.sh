334569134fe6643ec8ebf86cf7aa2bd682595002
chris <mail@chris007.de>
2019-01-08 20:39:40 +0100

    Do not run fbs installer for Linux build (no support yet)

--- linux-build/build.sh
+++ linux-build/build.sh
@@ -10,8 +10,5 @@ python3 -m pip install -r requirements.txt
 echo "# build the app"
 python3 -m fbs freeze
 
-echo "# create the installer"
-python3 -m fbs installer
-
 mkdir -p dist/release
 tar cfz dist/release/AYAB-Linux-$PACKAGE_VERSION.tar.gz -C target AYAB
