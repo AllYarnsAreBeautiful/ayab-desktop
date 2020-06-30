604b96281b6c78f33d39716b676435b24557ae65
Tom Price <t0mpr1c3@gmail.com>
2020-06-30 15:50:14 -0400

    added manual to Help - About form (#358)

--- src/main/python/ayab/ayab_about.py
+++ src/main/python/ayab/ayab_about.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ayab_about.ui'
+# Form implementation generated from reading ui file 'src/main/python/ayab/ayab_about.ui'
 #
-# Created: Thu Jan 26 21:51:16 2017
-#      by: PyQt5 UI code generator 5.3.2
+# Created by: PyQt5 UI code generator 5.14.2
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_AboutForm(object):
     def setupUi(self, AboutForm):
@@ -26,15 +27,15 @@ class Ui_AboutForm(object):
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
-        self.label_2 = QtWidgets.QLabel(AboutForm)
-        self.label_2.setObjectName("label_2")
-        self.verticalLayout.addWidget(self.label_2)
-        self.label_4 = QtWidgets.QLabel(AboutForm)
-        self.label_4.setObjectName("label_4")
-        self.verticalLayout.addWidget(self.label_4)
-        self.label_3 = QtWidgets.QLabel(AboutForm)
-        self.label_3.setObjectName("label_3")
-        self.verticalLayout.addWidget(self.label_3)
+        self.title_label = QtWidgets.QLabel(AboutForm)
+        self.title_label.setObjectName("title_label")
+        self.verticalLayout.addWidget(self.title_label)
+        self.link_label = QtWidgets.QLabel(AboutForm)
+        self.link_label.setObjectName("link_label")
+        self.verticalLayout.addWidget(self.link_label)
+        self.manual_label = QtWidgets.QLabel(AboutForm)
+        self.manual_label.setObjectName("manual_label")
+        self.verticalLayout.addWidget(self.manual_label)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem)
 
@@ -44,8 +45,6 @@ class Ui_AboutForm(object):
     def retranslateUi(self, AboutForm):
         _translate = QtCore.QCoreApplication.translate
         AboutForm.setWindowTitle(_translate("AboutForm", "About AYAB"))
-        self.label_2.setText(_translate("AboutForm", "All Yarns Are Beautiful"))
-        self.label_4.setText(_translate("AboutForm", "<html><head/><body><p><a href=\"http://ayab-knitting.com\"><span style=\" text-decoration: underline; color:#0000ff;\">http://ayab-knitting.com</span></a></p></body></html>"))
-        self.label_3.setText(_translate("AboutForm", "Version PACKAGE_VERSION"))
-
+        self.title_label.setText(_translate("AboutForm", "All Yarns Are Beautiful"))
+        self.link_label.setText(_translate("AboutForm", "<html><head/><body><p><a href=\"http://ayab-knitting.com\"><span style=\" text-decoration: underline; color:#0000ff;\">http://ayab-knitting.com</span></a></p></body></html>"))
 from . import resources_rc
