a76c279bab9d338b66940f410c246c5efe3aafb8
Thomas <mail@thomasdicke.de>
2023-10-29 10:33:14 +0100

    update windows build script, dll etc.

--- windows-build/build.bat
+++ windows-build/build.bat
@@ -1,9 +1,7 @@
 set VERSION=%1
 
-rem echo "# build the app"
 python -m fbs freeze
 
-rem echo "# create the installer"
 python -m fbs installer
 
-xcopy /Y  .\target\AYABSetup.exe .\target\AYAB-eKnitter-v%VERSION%-Setup.exe
+xcopy /Y  .\target\AYAB-eKnitterSetup.exe .\target\AYAB-eKnitter-v%VERSION%-Setup.exe
