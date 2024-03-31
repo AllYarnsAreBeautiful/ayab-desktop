748b868f3ac634c45de74c37110f4b0e966693b5
Sam! Bonfante <sam@bonfante.org>
2024-03-31 12:50:31 -0400

    update ayab_trans to run on windows

--- src/main/resources/base/ayab/translations/ayab_trans.pl
+++ src/main/resources/base/ayab/translations/ayab_trans.pl
@@ -15,6 +15,21 @@ use warnings;
 use List::Util qw(first);
 use File::Spec::Functions;
 
+#index, infile, outfile
+sub cut{
+open(my $infile, "<",$_[1]);
+open(my $outfile, ">",$_[2]);
+while (my $line = <$infile>) {
+	my @b = split(/\t/,$line);
+	print $outfile @b[$_[0]-1],"\n";
+}
+}
+
+#filenames
+sub rm{
+    unlink(@_)
+}
+
 my $master = "ayab-translation-master.tsv";
 open(FILE, "<", $master);
 chomp(my $line = <FILE>);
@@ -23,29 +38,29 @@ close(FILE);
 
 my $index = (first { $headers[$_] eq "Context" } 0 .. $#headers) + 1;
 my $filename = "ayab_trans_context.txt";
-system("cut -f$index $master > $filename");
+cut($index, $master, $filename);
 open(FILE, "<", $filename);
 chomp(my @context = <FILE>);
 close(FILE);
-system("rm $filename");
+rm($filename);
 
 $index += 1;
 $filename = "ayab_trans_base.txt";
-system("cut -f$index $master > $filename");
+cut($index, $master, $filename);
 open(FILE, "<", $filename);
 chomp(my @base = <FILE>);
 close(FILE);
-system("rm $filename");
+rm($filename);
 
 foreach my $column ($index .. $#headers){
         my $lang = $headers[$column-1];
         my $filename = "ayab_trans_$lang.txt";
-	system("cut -f$column $master > $filename");
+	cut($column, $master, $filename);
 	open(FILE, "<", $filename);
 	chomp(my @file = <FILE>);
 	close(FILE);
-	system("rm $filename");
-        $filename = "ayab_trans.$lang.ts";
+	rm($filename);
+    $filename = "ayab_trans.$lang.ts";
 	open(FILE, ">", $filename);
 	print FILE <<'HEADER';
 <?xml version="1.0" encoding="utf-8"?>
@@ -75,4 +90,4 @@ HEADER
 # run `lrelease *.ts` to create binary `.qm` files
 open(FILE, "<", $master);
 system("lrelease *.ts");
-system("rm -f *.ts");
+unlink glob "*.ts";
