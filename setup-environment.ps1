d2b25f8a0f25dfa566ee5eb73e10b91c1b043fc4
Sam! Bonfante <sam@bonfante.org>
2024-04-18 12:46:59 -0400

    fix module stuff MORE

--- setup-environment.ps1
+++ setup-environment.ps1
@@ -10,30 +10,30 @@ git submodule update --init --recursive
 pyside6-genpyi all
 
 # convert Qt `.ui` files to Python code
-pyside6-uic src/main/main/ayab/about_gui.ui -o src/main/main/ayab/about_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/about_gui.py
-pyside6-uic src/main/main/ayab/firmware_flash_gui.ui -o src/main/main/ayab/firmware_flash_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/firmware_flash_gui.py
-pyside6-uic src/main/main/ayab/main_gui.ui -o src/main/main/ayab/main_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/main_gui.py
-pyside6-uic src/main/main/ayab/menu_gui.ui -o src/main/main/ayab/menu_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/menu_gui.py
-pyside6-uic src/main/main/ayab/mirrors_gui.ui -o src/main/main/ayab/mirrors_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/mirrors_gui.py
-pyside6-uic src/main/main/ayab/prefs_gui.ui -o src/main/main/ayab/prefs_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/prefs_gui.py
-pyside6-uic src/main/main/ayab/engine/dock_gui.ui -o src/main/main/ayab/engine/dock_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/engine/dock_gui.py
-pyside6-uic src/main/main/ayab/engine/options_gui.ui -o src/main/main/ayab/engine/options_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/engine/options_gui.py
-pyside6-uic src/main/main/ayab/engine/status_gui.ui -o src/main/main/ayab/engine/status_gui.py
-perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/main/ayab/engine/status_gui.py
+pyside6-uic src/main/python/main/ayab/about_gui.ui -o src/main/python/main/ayab/about_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/about_gui.py
+pyside6-uic src/main/python/main/ayab/firmware_flash_gui.ui -o src/main/python/main/ayab/firmware_flash_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/firmware_flash_gui.py
+pyside6-uic src/main/python/main/ayab/main_gui.ui -o src/main/python/main/ayab/main_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/main_gui.py
+pyside6-uic src/main/python/main/ayab/menu_gui.ui -o src/main/python/main/ayab/menu_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/menu_gui.py
+pyside6-uic src/main/python/main/ayab/mirrors_gui.ui -o src/main/python/main/ayab/mirrors_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/mirrors_gui.py
+pyside6-uic src/main/python/main/ayab/prefs_gui.ui -o src/main/python/main/ayab/prefs_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/prefs_gui.py
+pyside6-uic src/main/python/main/ayab/engine/dock_gui.ui -o src/main/python/main/ayab/engine/dock_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/engine/dock_gui.py
+pyside6-uic src/main/python/main/ayab/engine/options_gui.ui -o src/main/python/main/ayab/engine/options_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/engine/options_gui.py
+pyside6-uic src/main/python/main/ayab/engine/status_gui.ui -o src/main/python/main/ayab/engine/status_gui.py
+perl -pi -e "s/^(.*def setupUi.*):$/\1->None:/s" src/main/python/main/ayab/engine/status_gui.py
 
 
 # generate PySide6 resource filea
-pyside6-rcc src/main/main/ayab/ayab_logo_rc.qrc -o src/main/main/ayab/ayab_logo_rc.py
-pyside6-rcc src/main/main/ayab/engine/lowercase_e_rc.qrc -o src/main/main/ayab/engine/lowercase_e_rc.py
-pyside6-rcc src/main/main/ayab/engine/lowercase_e_reversed_rc.qrc -o src/main/main/ayab/engine/lowercase_e_reversed_rc.py
+pyside6-rcc src/main/python/main/ayab/ayab_logo_rc.qrc -o src/main/python/main/ayab/ayab_logo_rc.py
+pyside6-rcc src/main/python/main/ayab/engine/lowercase_e_rc.qrc -o src/main/python/main/ayab/engine/lowercase_e_rc.py
+pyside6-rcc src/main/python/main/ayab/engine/lowercase_e_reversed_rc.qrc -o src/main/python/main/ayab/engine/lowercase_e_reversed_rc.py
 
 # generate translation files
 cd src/main/resources/base/ayab/translations/
